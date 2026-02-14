ee1b12de44082b02b8e52a14d6cfa517b94dc3a0
Sirzechs0 <jaysonapuya123@gmail.com>
2026-02-14 18:14:11 +0800

    first commit

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA0KuFigt75UqNmQ8Ze-gm_Qe8XC1VtfBA",
+    authDomain: "pcshs-sms.firebaseapp.com",
+    projectId: "pcshs-sms",
+    storageBucket: "pcshs-sms.firebasestorage.app",
+    messagingSenderId: "23508357934",
+    appId: "1:23508357934:web:7d11fe6b9327a2e74760bf",
+    measurementId: "G-LXR4QKXDME"
+  };
