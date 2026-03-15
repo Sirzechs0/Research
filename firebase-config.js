a5739299602818067e4eb7225f7393136918026a
Sirzechs0 <jaysonapuya123@gmail.com>
2026-03-15 19:56:29 +0800

    log in error

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA0KuFigt75UqNmQ8Ze-gm_Qe8XC1VtfBA",
   authDomain: "pcshs-sms.firebaseapp.com",
@@ -16,6 +11,7 @@ const firebaseConfig = {
   measurementId: "G-LXR4QKXDME"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export { auth, signInWithEmailAndPassword, onAuthStateChanged, signOut };
